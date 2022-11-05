cdb2f4fdb129352ffcab2c79a412de7742c29876
matiascabral <matiascabralmendez@gmail.com>
2022-11-05 17:00:46 -0300

    no se

--- product/mercadopago.ts
+++ product/mercadopago.ts
@@ -1,4 +1,11 @@
 export  function getInitPoint(cart,user, url) {
+ console.log(
+   JSON.stringify({
+     cart,
+     user,
+   })
+ );
+ 
   return fetch(url, {
     method: "POST",
     body: JSON.stringify({
