d39409007970c904d54b98ba6afd17974628ac3c
matiascabral <matiascabralmendez@gmail.com>
2022-11-05 18:36:10 -0300

    api prueba

---
+++ pages/api/prueba.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  console.log("entro a la api");
+  const body = req.body;
+  console.log("body:", body);
+  console.log("local_url:", process.env.LOCAL_URL);
+  try {
+   res.status(200).json(body);
+  } catch (error) {
+    console.log("error api: ", error);
+    res.status(500).json({ error: error });
+  }
+}
