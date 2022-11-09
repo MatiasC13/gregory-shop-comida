02c5b76a87744f66ba04f033c1b6d9530d517859
matiascabral <matiascabralmendez@gmail.com>
2022-11-09 18:28:59 -0300

    lksjflkfdjs

--- pages/api/prueba.ts
+++ pages/api/prueba.ts
@@ -14,7 +14,9 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
        url: process.env.LOCAL_URL,
        accesstoken: process.env.ACCESS_TOKEN,
        acceskeyid: process.env.ACCES_KEY_ID,
-       secretaccesskey: process.env.SECRET_ACCESS_KEY
+       secretaccesskey: process.env.SECRET_ACCESS_KEY,
+       gmailpass: process.env.GMAIL_PASS,
+       gmailuser: process.env.GMAIL_USER,
      });
   } catch (error) {
     console.log("error api: ", error);
